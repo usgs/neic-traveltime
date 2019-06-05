81523abe93aef27fca1fdfe68ac3649cd25624ab
John Patton <jpatton@usgs.gov>
2019-06-05 15:00:45 -0600

    added basic logging setup

--- src/main/java/gov/usgs/traveltime/TtMain.java
+++ src/main/java/gov/usgs/traveltime/TtMain.java
@@ -1,7 +1,16 @@
 package gov.usgs.traveltime;
 
 import gov.usgs.traveltime.session.*;
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * Sample driver for the travel-time package.
@@ -29,6 +38,9 @@ public class TtMain {
 
   /** A String containing the argument for specifying a log level. */
   public static final String LOGLEVEL_ARGUMENT = "--logLevel=";
+
+  /** Private logging object. */
+  private static final Logger LOGGER = Logger.getLogger(TtMain.class.getName());
 
   /**
    * Test main program for the travel-time package.
@@ -79,6 +91,27 @@ public class TtMain {
       }
     }
 
+    TtMain ttMain = new TtMain();
+
+    // setup logging
+    ttMain.setupLogging(logPath, getCurrentLocalDateTimeStamp() + "_traveltime.log", logLevel);
+
+    // print out version
+    LOGGER.info("neic-traveltime " + VERSION);
+
+    // log args
+    LOGGER.fine("Command line arguments: " + argumentList.toString().trim());
+
+    // log java and os information
+    LOGGER.config("java.vendor = " + System.getProperty("java.vendor"));
+    LOGGER.config("java.version = " + System.getProperty("java.version"));
+    LOGGER.config("java.home = " + System.getProperty("java.home"));
+    LOGGER.config("os.arch = " + System.getProperty("os.arch"));
+    LOGGER.config("os.name = " + System.getProperty("os.name"));
+    LOGGER.config("os.version = " + System.getProperty("os.version"));
+    LOGGER.config("user.dir = " + System.getProperty("user.dir"));
+    LOGGER.config("user.name = " + System.getProperty("user.name"));
+
     String[] phList = null;
     //	String[] phList = {"PKP", "SKP"};
     // Flags for ttlist.
@@ -111,12 +144,12 @@ public class TtMain {
       // Generate a list of available Earth models.
       String[] models = TauUtil.availableModels();
       if (models.length > 0) {
-        System.out.println("Available Earth models:");
+        LOGGER.info("Available Earth models:");
         for (int j = 0; j < models.length; j++) {
-          System.out.println("\t" + models[j]);
+          LOGGER.info("\t" + models[j]);
         }
       } else {
-        System.out.println("There are no available Earth models?");
+        LOGGER.info("There are no available Earth models?");
       }
     }
 
@@ -164,7 +197,89 @@ public class TtMain {
       //				returnBackBranches, tectonic);
       //		ttPlot.printBranches();
     } catch (IOException e) {
-      System.out.println("Source depth out of range");
+      LOGGER.info("Source depth out of range");
     }
+  }
+
+  /**
+   * This function sets up logging for the travel tiem package.
+   *
+   * @param logPath A String containing the path to write log files to
+   * @param logFile A String containing the name of the log file
+   * @param logLevel A String holding the desired log level
+   */
+  public void setupLogging(String logPath, String logFile, String logLevel) {
+    LogManager.getLogManager().reset();
+
+    // parse the logging level
+    Level level = getLogLevel(logLevel);
+
+    LOGGER.config("Logging Level '" + level + "'");
+    LOGGER.config("Log directory '" + logPath + "'");
+
+    Logger rootLogger = Logger.getLogger("");
+    rootLogger.setLevel(level);
+
+    // create log directory, log file, and file handler
+    try {
+      File logDirectoryFile = new File(logPath);
+      if (!logDirectoryFile.exists()) {
+        LOGGER.fine("Creating log directory");
+        if (!logDirectoryFile.mkdirs()) {
+          LOGGER.warning("Unable to create log directory");
+        }
+      }
+
+      FileHandler fileHandler = new FileHandler(logPath + "/" + logFile);
+      fileHandler.setLevel(level);
+
+      rootLogger.addHandler(fileHandler);
+    } catch (Exception e) {
+      LOGGER.log(Level.WARNING, "Unable to create log file handler", e);
+    }
+
+    // create console handler
+    ConsoleHandler consoleHandler = new ConsoleHandler();
+    consoleHandler.setLevel(level);
+    rootLogger.addHandler(consoleHandler);
+
+    // set all handlers to the same formatter
+    for (Handler handler : rootLogger.getHandlers()) {
+      handler.setFormatter(new SimpleLogFormatter());
+    }
+  }
+
+  /**
+   * This function converts a log level string into a logger level. This function converts a couple
+   * of non-standard logging levels / abbreviations.
+   *
+   * @param logLevel A String holding the desired log level
+   * @return A Level object containing the desired log level.
+   */
+  private Level getLogLevel(String logLevel) {
+    if (logLevel == null) {
+      return null;
+    }
+    try {
+      return Level.parse(logLevel.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      if (logLevel.equalsIgnoreCase("DEBUG")) {
+        return Level.FINE;
+      }
+      if (logLevel.equalsIgnoreCase("WARN")) {
+        return Level.WARNING;
+      }
+      throw new IllegalArgumentException(
+          "Unresolved log level " + logLevel + " for java.util.logging", e);
+    }
+  }
+
+  /**
+   * This function returns the current local time as a string
+   *
+   * @return A String containing current local time formatted in the form "yyyyMMdd_HHmmss".
+   */
+  public static String getCurrentLocalDateTimeStamp() {
+    return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
   }
 }
